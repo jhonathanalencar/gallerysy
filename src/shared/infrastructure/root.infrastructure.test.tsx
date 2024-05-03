75a429b0d5be71b7786cd7249ba45cca97c82495
jhonathanalencar <jhonathan.alencarfs@gmail.com>
2024-05-03 11:50:01 -0300

    test: add root infrastructure test

---
+++ src/shared/infrastructure/root.infrastructure.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react';
+
+import { RootInfrastructure } from './root.infrastructure';
+
+describe('<RootInfrastructure>', () => {
+  describe('Render', () => {
+    it('should render children prop when it is provided', () => {
+      const children = <div>Mikasa</div>;
+      render(<RootInfrastructure>{children}</RootInfrastructure>);
+
+      const text = screen.getByText(/mikasa/i);
+
+      expect(text).toBeInTheDocument();
+    });
+
+    it('should render nothing when children prop is null', () => {
+      const { container } = render(
+        <RootInfrastructure>{null}</RootInfrastructure>
+      );
+
+      expect(container).toBeEmptyDOMElement();
+    });
+  });
+});
