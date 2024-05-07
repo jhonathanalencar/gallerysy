724b2351ca9de88c48a2d5d1aed8d8b0c26e57ee
jhonathanalencar <jhonathan.alencarfs@gmail.com>
2024-05-06 22:52:45 -0300

    feat: update app layout

--- src/shared/layouts/app.layout.tsx
+++ src/shared/layouts/app.layout.tsx
@@ -1,11 +1,14 @@
+import { Header } from '../components/header.component';
+
 interface AppLayoutProps {
   children: React.ReactNode;
 }
 
 export function AppLayout({ children }: AppLayoutProps) {
   return (
-    <main className="flex min-h-full bg-zinc-900 text-zinc-100">
-      <div className="flex-1">{children}</div>
-    </main>
+    <div className="flex min-h-full flex-col bg-zinc-900 text-zinc-100">
+      <Header />
+      <main className="flex-1">{children}</main>
+    </div>
   );
 }
