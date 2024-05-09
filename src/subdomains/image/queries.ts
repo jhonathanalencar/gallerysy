1fde14440df56ef04c33bbc3a17b784fc642c519
jhonathanalencar <jhonathan.alencarfs@gmail.com>
2024-05-09 17:07:31 -0300

    feat: update get images query and get image query

--- src/subdomains/image/queries.ts
+++ src/subdomains/image/queries.ts
@@ -1,11 +1,15 @@
 'server-only';
 import { cache } from 'react';
+import { auth } from '@clerk/nextjs/server';
 
 import { db } from '@externals/storage/connection.storage';
 import { addBlurredDataUrls } from '@shared/helpers/add-blurred-data-urls';
 
 export const getImages = cache(async () => {
+  const session = auth();
+  if (!session.userId) throw new Error('Unauthenticated');
   const imagesData = await db.query.image.findMany({
+    where: (model, { eq }) => eq(model.userId, session.userId),
     orderBy: (model, { desc }) => desc(model.imageId),
     limit: 12,
   });
@@ -13,9 +17,12 @@ export const getImages = cache(async () => {
   return images;
 });
 
-export const getImage = cache(async (imageId: number) => {
+export const getImage = cache(async (imageId: number, userId: string) => {
+  const session = auth();
+  if (!session.userId) throw new Error('Unauthenticated');
   const imagesData = await db.query.image.findFirst({
-    where: (model, { eq }) => eq(model.imageId, imageId),
+    where: (model, { eq, and }) =>
+      and(eq(model.imageId, imageId), eq(model.userId, userId)),
   });
   if (!imagesData) throw new Error('Image not found');
   const [image] = await addBlurredDataUrls([imagesData]);
