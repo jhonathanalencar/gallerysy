1204ec768c7b85ad0dfee81ef7ead2a8b3b95cf6
jhonathanalencar <jhonathan.alencarfs@gmail.com>
2024-05-09 22:34:11 -0300

    feat: update upload button

--- src/shared/components/upload-button.component.tsx
+++ src/shared/components/upload-button.component.tsx
@@ -1,16 +1,79 @@
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 import { toast } from 'sonner';
+import { UploadIcon } from 'lucide-react';
 
-import { UploadButton as UtUploadButton } from '../libs/uploadthing.lib';
+import { useUploadThing } from '../libs/uploadthing.lib';
+
+type Input = Parameters<typeof useUploadThing>;
+
+const useUploadThingInputProps = (...args: Input) => {
+  const $ut = useUploadThing(...args);
+
+  const onChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (!event.target.files) return;
+
+    const selectedFiles = Array.from(event.target.files);
+    const result = await $ut.startUpload(selectedFiles);
+
+    console.log('uploaded files', result);
+  };
+
+  return {
+    inputProps: {
+      onChange,
+      multiple: ($ut.permittedFileInfo?.config?.image?.maxFileCount ?? 1) > 1,
+      accept: 'image/*',
+    },
+    isUploading: $ut.isUploading,
+  };
+};
 
 export function UploadButton() {
+  const [, setImages] = useState<
+    {
+      fileUrl: string;
+      fileKey: string;
+    }[]
+  >([]);
   const router = useRouter();
 
-  return (
-    <UtUploadButton
-      className="ut-button:bg-primary ut-button:font-bold ut-button:text-background ut-button:ring-primary ut-button:ring-offset-zinc-950 hover:ut-button:brightness-110 ut-allowed-content:text-foreground ut-uploading:text-background ut-button:ut-uploading:cursor-not-allowed ut-button:ut-uploading:bg-secondary"
-      endpoint="imageUploader"
-      onClientUploadComplete={() => {
+  const { inputProps, isUploading } = useUploadThingInputProps(
+    'imageUploader',
+    {
+      onUploadBegin() {
+        toast.loading('Uploading...', {
+          position: 'top-right',
+          id: 'upload-started',
+          style: {
+            backgroundColor: 'hsl(20 14.3% 4.1%)',
+            color: 'hsl(20.5 90.2% 48.2%)',
+            borderColor: 'hsl(12 6.5% 15.1%)',
+          },
+        });
+      },
+      onUploadError(error) {
+        console.error(`ERROR! ${error.message}`);
+        toast.dismiss('upload-started');
+        toast.error(error.message || 'Failed to upload file', {
+          position: 'top-right',
+          style: {
+            backgroundColor: 'hsl(20 14.3% 4.1%)',
+            color: 'hsl(20.5 90.2% 48.2%)',
+            borderColor: 'hsl(12 6.5% 15.1%)',
+          },
+        });
+      },
+      onClientUploadComplete(res) {
+        setImages(
+          res.map((file) => {
+            return {
+              fileKey: file.key,
+              fileUrl: file.url,
+            };
+          })
+        );
+        toast.dismiss('upload-started');
         toast.success('Upload Completed', {
           position: 'top-right',
           style: {
@@ -20,18 +83,26 @@ export function UploadButton() {
           },
         });
         router.refresh();
-      }}
-      onUploadError={(error) => {
-        console.error(`ERROR! ${error.message}`);
-        toast.error(error.message || 'Failed to upload file', {
-          position: 'top-right',
-          style: {
-            backgroundColor: 'hsl(20 14.3% 4.1%)',
-            color: 'hsl(20.5 90.2% 48.2%)',
-            borderColor: 'hsl(12 6.5% 15.1%)',
-          },
-        });
-      }}
-    />
+      },
+    }
+  );
+
+  return (
+    <div>
+      <label
+        htmlFor="upload"
+        className="flex cursor-pointer items-center gap-2 rounded bg-black/40 px-4 py-2 focus-within:ring-2 focus-within:ring-primary focus-within:ring-offset-2 focus-within:ring-offset-zinc-900 hover:bg-black/60 has-[:disabled]:cursor-not-allowed has-[:disabled]:bg-black/40"
+      >
+        <UploadIcon className="h-5 w-5 text-orange-400" />
+        <span>Upload</span>
+        <input
+          id="upload"
+          type="file"
+          disabled={isUploading}
+          className="peer sr-only"
+          {...inputProps}
+        />
+      </label>
+    </div>
   );
 }
