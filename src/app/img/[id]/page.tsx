22ff331a08082811766757db1189deee25f3983f
jhonathanalencar <jhonathan.alencarfs@gmail.com>
2024-05-11 12:52:42 -0300

    chore: add dynamic metadata

--- src/app/img/[id]/page.tsx
+++ src/app/img/[id]/page.tsx
@@ -1,8 +1,31 @@
+import type { Metadata } from 'next';
+import { notFound } from 'next/navigation';
+
 import { imageRoutes } from '@subdomains/image/routes';
+import { getImage } from '@subdomains/image/queries';
 
 interface PicturePageProps {
   params: {
     id: string;
+  };
+}
+
+export async function generateMetadata({
+  params: { id },
+}: PicturePageProps): Promise<Metadata> {
+  const imageIdAsNumber = parseInt(id);
+  const image = await getImage(imageIdAsNumber);
+  if (!image) return notFound();
+  return {
+    title: image.name,
+    description: `Picture of ${image.name}`,
+    openGraph: {
+      images: [
+        {
+          url: image.imageUrl,
+        },
+      ],
+    },
   };
 }
 
